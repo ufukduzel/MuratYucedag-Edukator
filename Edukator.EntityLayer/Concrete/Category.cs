23742643083d980cda5d12b6ee25e6d3a46b2607
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-05-22 00:44:04 +0300

    Add project files.

---
+++ Edukator.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+    // Amacımız Code First çalışmak
+    // Sınıfları Visual Studio da yazacağız, bunları SQL de bir tabloya dönüştüreceğiz
+    public class Category
+    {
+        // değişken
+        //int x; // x e bir kısıtlama yapamayız
+
+        // property (nitelik, özellik)
+        //int y { get; set; } // y ye bir kısıtlama yapabiliriz
+        // y için sadece 10 20 aralığında ol veya negatif değerleri alma kısıtlaması yapabiliriz
+
+        public int CategoryID { get; set; }
+        // Bu yazılış, sınıfın birincil ID si ve otomatik artan formatta olduğunu belirtiyor
+
+        public string CategoryName { get; set; }
+    }
+}
